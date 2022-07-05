bdcdb7e1a9ad557c0e99cc3b3b0343ab04e61c8e
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-07-05 20:55:16 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: chenjie
+ * @Date: 2022-07-05 20:28:09
+ * @LastEditors: chenjie
+ * @LastEditTime: 2022-07-05 20:43:33
+ * @FilePath: \react-geekh5-ts\src\App.tsx
+ * @Description: App
+ * Copyright (c) 2022 by chenjie, All Rights Reserved.
+ */
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      GEEK
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+/*
+ * @Author: chenjie
+ * @Date: 2022-07-05 20:28:09
+ * @LastEditors: chenjie
+ * @LastEditTime: 2022-07-05 20:42:16
+ * @FilePath: \react-geekh5-ts\src\index.tsx
+ * @Description: 
+ * Copyright (c) 2022 by chenjie, All Rights Reserved.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
