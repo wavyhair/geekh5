f7b4d2d9daa19acaf1f917700c8725d574989814
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-07-09 21:37:07 +0800

    安装pnpm

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@
  * @Author: chenjie
  * @Date: 2022-07-05 20:28:09
  * @LastEditors: chenjie
- * @LastEditTime: 2022-07-05 21:06:56
+ * @LastEditTime: 2022-07-09 20:56:58
  * @FilePath: \react-geekh5-ts\src\App.tsx
  * @Description: App
  * Copyright (c) 2022 by chenjie, All Rights Reserved.
  */
-import { BrowserRouter as Router, Route, Switch,Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Login from './pages/Login';
 import './App.scss';
@@ -15,11 +15,11 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/" exact render={()=><Redirect to="/home"></Redirect>}></Route>
-          <Route path="/home" component={Layout}></Route>
-          <Route path="/login" component={Login}></Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />}></Route>
+          <Route path="/home" element={<Layout />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
       </Router>
     </div>
   );
--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -2,14 +2,16 @@
  * @Author: chenjie
  * @Date: 2022-07-05 21:00:44
  * @LastEditors: chenjie
- * @LastEditTime: 2022-07-05 21:01:26
+ * @LastEditTime: 2022-07-09 21:33:23
  * @FilePath: \react-geekh5-ts\src\pages\Layout\index.tsx
  * @Description: Layout
  * Copyright (c) 2022 by chenjie, All Rights Reserved.
  */
-
+import { Button } from 'antd-mobile'
 export default function Layout() {
   return (
-    <div>Layout</div>
+    <div>
+      <Button>test</Button>
+    </div>
   )
 }
