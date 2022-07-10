031965062c4230dc0a2e46f5ba51e1463c2a41da
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-07-10 13:36:00 +0800

    解决移动端1px像素问题

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -2,16 +2,19 @@
  * @Author: chenjie
  * @Date: 2022-07-05 21:00:44
  * @LastEditors: chenjie
- * @LastEditTime: 2022-07-09 21:33:23
+ * @LastEditTime: 2022-07-10 13:16:37
  * @FilePath: \react-geekh5-ts\src\pages\Layout\index.tsx
  * @Description: Layout
  * Copyright (c) 2022 by chenjie, All Rights Reserved.
  */
 import { Button } from 'antd-mobile'
+import styles from './index.module.scss'
 export default function Layout() {
   return (
-    <div>
+    <div className={styles.root}>
       <Button>test</Button>
+      <div className='div-test'></div>
+      <div className='line'></div>
     </div>
   )
 }
