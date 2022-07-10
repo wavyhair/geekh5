e49cb349be31aadd6351670903fbf966be224e72
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-07-10 10:17:18 +0800

    移动端适配

--- craco.config.js
+++ craco.config.js
@@ -2,12 +2,17 @@
  * @Author: chenjie
  * @Date: 2022-07-10 08:54:47
  * @LastEditors: chenjie
- * @LastEditTime: 2022-07-10 08:57:16
+ * @LastEditTime: 2022-07-10 10:04:04
  * @FilePath: \react-geekh5-ts\craco.config.js
  * @Description: 配置文件中配置路径别名
  * Copyright (c) 2022 by chenjie, All Rights Reserved.
  */
 const path = require('path')
+const pxToViewport = require('postcss-px-to-viewport');
+const vw = pxToViewport({
+    // 视口宽度，一般就是 375（ 设计稿一般采用二倍稿，宽度为 375 ）
+    viewportWidth: 375,
+  });
 module.exports = {
     webpack:{
         alias:{
@@ -15,6 +20,11 @@ module.exports = {
             '@':path.resolve(__dirname,'src'),
             // 使用 @scss 表示全局 SASS 样式所在路径
             '@scss':path.resolve(__dirname,'src/assets/styles')
+        }
+    },
+    style:{
+        postcss:{
+            plugins:[vw]
         }
     }
 }
