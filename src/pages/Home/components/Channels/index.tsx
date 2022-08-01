6d7a9fa9126506abf52734cefd4e7140a8654ca7
‘githahahaha’ <did_not_eat_enough@yeah.net>
2022-08-01 20:40:53 +0800

    删除频道bugfix

--- src/pages/Home/components/Channels/index.tsx
+++ src/pages/Home/components/Channels/index.tsx
@@ -2,13 +2,13 @@
  * @Author: chenjie
  * @Date: 2022-08-01 14:51:11
  * @LastEditors: chenjie
- * @LastEditTime: 2022-08-01 17:41:51
+ * @LastEditTime: 2022-08-01 20:13:44
  * @Description: Channels
  */
 import classnames from 'classnames'
 import Icon from '@/components/Icon'
 import styles from './index.module.scss'
-import {useAppDispatch, useAppSelector} from "@/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import {
     getAllChannel,
     selectChannelActiveKey,
@@ -17,15 +17,15 @@ import {
     changeTab,
     delChannel
 } from "@/store/festures/home-slice";
-import {useInitialState} from "@/utils/use-initial-state";
-import {Channel} from "@/types/data";
-import {useState} from "react";
+import { useInitialState } from "@/utils/use-initial-state";
+import { Channel } from "@/types/data";
+import { useState } from "react";
 
 
 type Props = {
     onClose: () => void
 }
-const Channels = ({onClose}: Props) => {
+const Channels = ({ onClose }: Props) => {
     const dispatch = useAppDispatch()
     const channels = useAppSelector(selectChannels)
     const [isEdit, setIsEdit] = useState(false)
@@ -44,7 +44,7 @@ const Channels = ({onClose}: Props) => {
     return (
         <div className={styles.root}>
             <div className="channel-header">
-                <Icon type="iconbtn_channel_close" onClick={onClose}/>
+                <Icon type="iconbtn_channel_close" onClick={onClose} />
             </div>
             <div className="channel-content">
                 {/* 编辑时，添加类名 edit */}
@@ -53,18 +53,18 @@ const Channels = ({onClose}: Props) => {
                         <span className="channel-item-title">我的频道</span>
                         <span className="channel-item-title-extra">点击进入频道</span>
                         <span className="channel-item-edit"
-                              onClick={() => setIsEdit(!isEdit)}>{isEdit ? '保存' : '编辑'}</span>
+                            onClick={() => setIsEdit(!isEdit)}>{isEdit ? '保存' : '编辑'}</span>
                     </div>
                     <div className="channel-list">
                         {/* 选中时，添加类名 selected */}
                         {channels.map(channle =>
-                                <span
-                                    className={classnames('channel-list-item', (channelActiveKey === channle.id + '') && 'selected')}
-                                    onClick={() => handleChangeTab(channle)} key={channle.id}>
-              {channle.name}
-                                    {channle.id !== 0  && <Icon type="iconbtn_tag_close"/>}
+                            <span
+                                className={classnames('channel-list-item', (channelActiveKey === channle.id + '') && 'selected')}
+                                onClick={() => handleChangeTab(channle)} key={channle.id}>
+                                {channle.name}
+                                {channle.id !== 0 && channels.length > 4 && <Icon type="iconbtn_tag_close" />}
 
-            </span>
+                            </span>
                         )}
 
                     </div>
@@ -77,7 +77,7 @@ const Channels = ({onClose}: Props) => {
                     </div>
                     <div className="channel-list">
                         {restChannels.map((item) => <span className="channel-list-item"
-                                                          key={item.id}>+ {item.name}</span>)
+                            key={item.id}>+ {item.name}</span>)
                         }
                     </div>
                 </div>
